07337b4201ca1a9e24fb54df89b7979d573779ef
Nathan Brooks <nbbrooks@gmail.com>
2015-03-25 14:53:03 +0300

    - Moved DreaamHelper into CoreHelper to avoid dependencies issues

--- src/sami/CoreHelper.java
+++ src/sami/CoreHelper.java
@@ -1,5 +1,7 @@
 package sami;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author nbb
@@ -26,6 +28,35 @@ public class CoreHelper {
             }
         }
         return reduced;
+    }
+
+    public static String getUniqueName(String name, ArrayList<String> existingNames) {
+        boolean invalidName = existingNames.contains(name);
+        while (invalidName) {
+            int index = name.length() - 1;
+            if ((int) name.charAt(index) < (int) '0' || (int) name.charAt(index) > (int) '9') {
+                // name does not end with a number - attach a "2"
+                name += "2";
+            } else {
+                // Find the number the name ends with and increment it
+                int numStartIndex = -1, numEndIndex = -1;
+                while (index >= 0) {
+                    if ((int) name.charAt(index) >= (int) '0' && (int) name.charAt(index) <= (int) '9') {
+                        if (numEndIndex == -1) {
+                            numEndIndex = index;
+                        }
+                    } else if (numEndIndex != -1) {
+                        numStartIndex = index + 1;
+                        break;
+                    }
+                    index--;
+                }
+                int number = Integer.parseInt(name.substring(numStartIndex, numEndIndex + 1));
+                name = name.substring(0, numStartIndex) + (number + 1);
+            }
+            invalidName = existingNames.contains(name);
+        }
+        return name;
     }
 
 }
